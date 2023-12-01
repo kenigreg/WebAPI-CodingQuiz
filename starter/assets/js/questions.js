ea95b96c925cc8683722dc279660136122d93f79
Gregory Oghenah <kenny@gregorys-mbp.home>
2023-12-01 21:12:04 +0000

    Added the JS files and updated the Questions js file

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,31 @@
+const questions = [
+    {
+        word: 'A string in JavaScript needs to be written inside ___________.',
+        answerOptions: ['Quotes', 'Curly braces', 'Asterix', 'Square brackets'],
+        correctAnswer: 'Quotes'
+    },
+
+    {
+        word: 'Using a const variable before it is declared, gives ___________.',
+        answerOptions: ['a Reference Error', 'Undefined', 'a Syntax Error', 'Null'],
+        correctAnswer: 'a Syntax Error'
+    },
+
+    {
+        word: 'Using a let variable before it is declared will result in ___________.',
+        answerOptions: ['Undefined', 'Null', 'a Syntax Error', 'a Reference Error'],
+        correctAnswer: 'a Reference Error'
+    },
+
+    {
+        word: 'JSON stands for ___________.',
+        answerOptions: ['JavaScript Only Negative', 'JavaScript Object Notation', 'JavaScript Object Negation', 'JavaScript Occuring Nodes'],
+        correctAnswer: 'JavaScript Object Notation'
+    }
+
+];
+
+
+
+
+
