f16b89c04491c14952582a8dcb2c1238443f1eeb
Gregory Oghenah <kenny@gregorys-mbp.home>
2023-12-03 21:21:26 +0000

    Completed the function to display each question and answer options

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,30 +1,49 @@
 //Quiz Questions
 const quizQuestions = questions;
 
+//Element Variables
 let getCurrentTime = document.getElementById("time");
 const getQuestionEle = document.getElementById("question-title");
 let getChoices = document.getElementById("choices");
+let displayQuestions = document.getElementById("questions");
+
+//create an ol element and append to the DIV element
+const orderList = document.createElement("ol");
+getChoices.appendChild(orderList);
 
 
+
+//Event Listener for startQuiz Button
 document.getElementById("start").addEventListener("click", startQuiz);
 
 let counter = 75;
 
 //function to listen to click event on Start Quiz button
-function startQuiz(e) {
-    e.preventDefault();
+function startQuiz() {
     // Update the count down every 1 second
     const x = setInterval(function () {
         let countTimer = counter--;
          getCurrentTime.innerText = countTimer
-        console.log(countTimer);
+        //console.log(countTimer);
     
-        if (countTimer <= 1) {
+        if (countTimer <= 0) {
           clearInterval(x);
-          getCurrentTime.innerText = counter;
+          getCurrentTime.innerText = 75;
         }
     }, 1000);
-    
+    displayQuizQuestions();
+};
 
-    
-}
+function displayQuizQuestions() {
+    let index = Math.floor(Math.random() * quizQuestions.length);
+
+    getQuestionEle.textContent = quizQuestions[index].word
+
+    quizQuestions[index].answerOptions.forEach((option, j) => {
+        const itemList = document.createElement("li");
+        itemList.setAttribute("id", j);
+        itemList.textContent = option;
+        orderList.appendChild(itemList);
+    });  
+    displayQuestions.setAttribute("class", "block");
+};
